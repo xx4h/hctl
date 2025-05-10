b848d8dc53c0aa9e6e74c7f51f3454062c82630e
Adam <adam.wyatt@iinet.net.au>
2025-05-10 09:38:21 +0800

    Change from 'color_temp' to 'color-temp'

--- cmd/on.go
+++ cmd/on.go
@@ -30,7 +30,7 @@ func newOnCmd(h *pkg.Hctl, out io.Writer) *cobra.Command {
 	var colorTemp int
 
 	cmd := &cobra.Command{
-		Use:   "on [-b|--brightness +|-|min|max|1-99] [-c|--color R,G,B] [-t|--color_temp 153-500]",
+		Use:   "on [-b|--brightness +|-|min|max|1-99] [-c|--color R,G,B] [-t|--color-temp 153-500]",
 		Short: "Switch or turn on a light or switch",
 		Args:  cobra.MatchAll(cobra.ExactArgs(1)),
 		ValidArgsFunction: func(_ *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
@@ -71,7 +71,7 @@ func newOnCmd(h *pkg.Hctl, out io.Writer) *cobra.Command {
 
 	cmd.PersistentFlags().StringVarP(&brightness, "brightness", "b", "", "Set brightness")
 	cmd.PersistentFlags().StringVarP(&color, "color", "c", "", "Set RGB color in format R,G,B")
-	cmd.PersistentFlags().IntVarP(&colorTemp, "color_temp", "t", 0, "Set color temperature in mireds (153-500)")
+	cmd.PersistentFlags().IntVarP(&colorTemp, "color-temp", "t", 0, "Set color temperature in mireds (153-500)")
 	err := cmd.RegisterFlagCompletionFunc("brightness", func(_ *cobra.Command, _ []string, _ string) ([]string, cobra.ShellCompDirective) {
 		return brightnessRange, cobra.ShellCompDirectiveKeepOrder | cobra.ShellCompDirectiveNoFileComp
 	})
